76b6190435f43883ad8541a1a5ae8093383222ee
Anton Litus <littox90@gmail.com>
2020-05-01 12:54:18 +0300

    8 sec

--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"sort"
+	"strings"
+	"time"
+
 	//"runtime"
 	"strconv"
 	"sync"
@@ -13,14 +17,15 @@ func ExecutePipeline(jobs ...job) {
 	wgm.Add(len(jobs))
 
 	var out chan interface{}
-	in := make(chan interface{}, 1)
+	in := make(chan interface{}, 100)
 
 	for _, fn := range jobs {
-		out = make(chan interface{})
+		out = make(chan interface{}, 100)
 
 		go func(f job, wgm *sync.WaitGroup, in, out chan interface{}) {
 			defer wgm.Done()
 			defer close(out)
+			fmt.Println("run")
 			f(in, out)
 		}(fn, wgm, in, out)
 
@@ -31,74 +36,52 @@ func ExecutePipeline(jobs ...job) {
 
 var SingleHash = func(in, out chan interface{}) {
 	for dataRaw := range in {
-		data, ok := dataRaw.(string)
-		if !ok {
-			fmt.Printf("cant convert result data to string")
-		}
+		//dataRaw := <-in
+		data := strconv.Itoa(dataRaw.(int))
+
 		md5chan := make(chan string)
 		crc32md5chan := make(chan string)
 		crc32chan := make(chan string)
-		go putCrc32ToChan(crc32chan, data)
-		go putMd5ToChan(md5chan, data)
-		go putCrc32ToChan(crc32md5chan, <-md5chan)
+		go func(md5chan,
+			crc32md5chan,
+			crc32chan chan string) {
+			go putCrc32ToChan(crc32chan, data)
+			go putMd5ToChan(md5chan, data)
+			go putCrc32ToChan(crc32md5chan, <-md5chan)
+		}(md5chan,
+			crc32md5chan,
+			crc32chan)
 
 		out <- <-crc32chan + "~" + <-crc32md5chan
 	}
 }
-
 var MultiHash = func(in, out chan interface{}) {
-
-	const count = 5
-
 	for dataRaw := range in {
+
+		//dataRaw := <-in
 		data, ok := dataRaw.(string)
 		if !ok {
 			fmt.Printf("cant convert result data to string")
 		}
 
-		parts := make(chan string)
+		const count = 5
+		var counters = map[int]string{}
+		mu := &sync.Mutex{}
+		wg := &sync.WaitGroup{}
 
-		go func(cnt int, data string, parts chan string) {
-			for i := 0; i <= count; i++ {
-				parts <- DataSignerCrc32(strconv.Itoa(i) + data)
-			}
-			close(parts)
-		}(count, data, parts)
+		for i := 0; i <= count; i++ {
+			wg.Add(1)
+			go putMultihashPartToMap(counters, i, data, mu, wg)
+		}
 
-		go func(parts chan string, out chan interface{}) {
-			var res string
-			for val := range parts {
-				res += val
-			}
-			out <- res
-		}(parts, out)
+		wg.Wait()
+
+		out <- getMultihash(counters, count)
 	}
 }
-var MultiHash = func(in, out chan interface{}) {
 
-	dataRaw := <-in
-	data, ok := dataRaw.(string)
-	if !ok {
-		fmt.Printf("cant convert result data to string")
-	}
-
-	const count = 5
-	var counters = map[int]string{}
-	mu := &sync.Mutex{}
-	wg := &sync.WaitGroup{}
-
-	for i := 0; i <= count; i++ {
-		wg.Add(1)
-		go putMultihashPartToMap(counters, i, data, mu, wg)
-	}
-
-	wg.Wait()
-
-	out <- getMultihash(counters, count)
-}
-
+//
 func getMultihash(counters map[int]string, count int) string {
-
 	var res string
 	for i := 0; i <= count; i++ {
 		res += counters[i]
@@ -123,78 +106,19 @@ func putMd5ToChan(ch chan string, data string) {
 }
 
 var CombineResults = func(in, out chan interface{}) {
-}
-
-func main() {
-
-	for i := 0; i < 5; i++ {
-		k := make(chan int)
-		go func(ch chan int) {
-			fmt.Println(i, ch, <-ch)
-		}(k)
-		k <- i + 1
+	var all []string
+	start := time.Now()
+	for dataRaw := range in {
+		data, ok := dataRaw.(string)
+		if !ok {
+			fmt.Printf("cant convert result data to string CombineResults")
+		}
+		all = append(all, data)
 	}
-	//start := time.Now()
-	//ch1 := make(chan interface{})
-	//ch2 := make(chan interface{})
-	////go MultiHash(ch1, ch2)
-	//go SingleHash(ch1, ch2)
-	//ch1 <- "hash"
-	//res := <-ch2
-
-	fmt.Scanln()
-	//end := time.Since(start)
-	//fmt.Println(res, end)
+	end := time.Since(start)
+	sort.Slice(all, func(i, j int) bool {
+		return all[i] < all[j]
+	})
+	out <- strings.Join(all, "_")
+	fmt.Println("exec time multihash", end)
 }
-
-//
-//
-//type job func(in, out chan interface{})
-//
-//const (
-//	MaxInputDataLen = 100
-//)
-//
-//var (
-//	dataSignerOverheat uint32 = 0
-//	DataSignerSalt            = ""
-//)
-//
-//var OverheatLock = func() {
-//	for {
-//		if swapped := atomic.CompareAndSwapUint32(&dataSignerOverheat, 0, 1); !swapped {
-//			fmt.Println("OverheatLock happend")
-//			time.Sleep(time.Second)
-//		} else {
-//			break
-//		}
-//	}
-//}
-//
-//var OverheatUnlock = func() {
-//	for {
-//		if swapped := atomic.CompareAndSwapUint32(&dataSignerOverheat, 1, 0); !swapped {
-//			fmt.Println("OverheatUnlock happend")
-//			time.Sleep(time.Second)
-//		} else {
-//			break
-//		}
-//	}
-//}
-//
-//var DataSignerMd5 = func(data string) string {
-//	OverheatLock()
-//	defer OverheatUnlock()
-//	data += DataSignerSalt
-//	dataHash := fmt.Sprintf("%x", md5.Sum([]byte(data)))
-//	time.Sleep(10 * time.Millisecond)
-//	return dataHash
-//}
-//
-//var DataSignerCrc32 = func(data string) string {
-//	data += DataSignerSalt
-//	crcH := crc32.ChecksumIEEE([]byte(data))
-//	dataHash := strconv.FormatUint(uint64(crcH), 10)
-//	time.Sleep(time.Second)
-//	return dataHash
-//}
